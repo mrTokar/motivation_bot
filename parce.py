23fcd4e005884f13e5e2308353996631d591834d
Egor Tokarev <egortok2014@gmail.com>
2025-11-02 01:20:17 +0300

    Remove function parce_block (no_used)

--- parce.py
+++ parce.py
@@ -1,17 +1,5 @@
 import json
 
-def parce_blocks(filename: str, sep_line: str):
-    """Return text between `sep_line`"""
-    with open(filename, encoding="utf-8") as file:
-        text = ''
-        for line in file:
-            if (line == sep_line + '\n') or (line == sep_line and text):
-                yield text
-                text = ''
-            else:
-                text += line
-        if text: yield text
-
 def get_messages_from_json(filename:str):
     """Return cortege (id, meassage) from `filename` in json format"""
     with open(filename, encoding="utf-8") as file:
@@ -35,7 +23,6 @@ def get_messages(filename: str, sep_line: str):
             else:
                 message += line
         if vk_id is not None: yield vk_id, message.strip()  
-
 
 if __name__ == "__main__":
    for vk_id, text in get_messages_from_json("source\motivational_messages.json"):
