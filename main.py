9c557dd72ce730db449a2de0460afe22fa8ba3ac
Egor Tokarev <egortok2014@gmail.com>
2025-11-02 00:05:29 +0300

    Add check data before sending

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from vk_bot import VkBot
 from parce import get_messages
 from dotenv import load_dotenv
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 class MotivationBot:
     motivation_path = ""
+    photo_path = ""
     sep_line = "="*80
 
     def __init__(self):
@@ -14,25 +15,30 @@ class MotivationBot:
         load_dotenv()
         self.bot = VkBot()
     
-    def get_file_path(self):
+    def _get_file_path(self):
         print("Загрузите индивидуальные мотивационнаые сообщения...")
         self.motivation_path = gui.select_file("с мотивационными сообщениями")
         if (gui.request("Хотите заменить разделяющую строку?", default_answer=False)):
             self.sep_line = input("Введите новую разделяющую строку: ")
 
-    def send_messages(self):
+    def _send_messages(self):
         for vk_id, message in get_messages(self.motivation_path, self.sep_line):
             self.bot.send_message(vk_id, message)
             print(f"Sent to {vk_id}")
     
     def run(self):
-        if self.motivation_path == "":
-            self.get_file_path()
-        if gui.request("Хотите загрузить изображение?"):
-            self.bot.upload_photo(gui.select_file("изображения"))
-        
-        self.send_messages()
+        while True:
+            if self.motivation_path == "":
+                self._get_file_path()
+            if gui.request("Хотите загрузить изображение?"):
+                self.photo_path = gui.select_file("изображения")
+                self.bot.upload_photo(self.photo_path)
+            print()
             
+            print(f"Загруженные данные: \n-текст: {self.motivation_path} \n-изображение: {self.photo_path}")
+            if gui.request("Все верно? Отправляем сообщения?"): break
+
+        self._send_messages()   
     
 if __name__ == "__main__":
     MotivationBot().run()
